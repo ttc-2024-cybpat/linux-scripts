ad739d69c6bd93c7b68273ba9e378a5adff2da1d
UnusualEgg <shotgun282006@gmail.com>
2024-10-01 18:25:40 +0000

    pam

--- script.sh
+++ script.sh
@@ -9,6 +9,15 @@ sudo passwd -l root
 #check for nopasswd
 # remove lines with these
 # is this safe though?
-sed -i 's/.*(nopasswd|!authenticate).*//' /etc/sudoers
+sudo sed -i 's/.*(nopasswd|!authenticate).*//' /etc/sudoers
 
+#set pam remember to 14
+#try to replace the current value
+old=$(< /etc/pam.d/common-password) 
+sudo sed -i 's/pam_history[  ]\+remember[ ]*=[ ]*[0-9]*/pam_history.so remember=14' /etc/pam.d/common-password
+new=$(< /etc/pam.d/common-password) 
+if [ "$new" == "$old" ];
+then
+  sudo echo "pam_history.so remember=14" >> /etc/pam.d/common-password
+fi
 
